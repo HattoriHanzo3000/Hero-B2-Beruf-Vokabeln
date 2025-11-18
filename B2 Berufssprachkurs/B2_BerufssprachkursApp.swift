c2c9000e3bd033022229b666ad30c6a5a4fda94b
Ildar Gizatullin <gizatullin.ildar@gmail.com>
2025-11-18 21:27:04 +0100

    Initial Commit

---
+++ B2 Berufssprachkurs/B2_BerufssprachkursApp.swift
@@ -0,0 +1,17 @@
+//
+//  B2_BerufssprachkursApp.swift
+//  B2 Berufssprachkurs
+//
+//  Created by Ildar on 18.11.25.
+//
+
+import SwiftUI
+
+@main
+struct B2_BerufssprachkursApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
