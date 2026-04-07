f1682ffac2cef1d1a9a228ee7d175da7ca42be3f
Ildar Gizatullin <gizatullin.ildar@gmail.com>
2026-04-07 20:47:13 +0200

    Add HeroB2Widgets

---
+++ HeroB2Widgets/HeroB2WidgetsBundle.swift
@@ -0,0 +1,17 @@
+//
+//  HeroB2WidgetsBundle.swift
+//  HeroB2Widgets
+//
+//  Created by Illo on 07.04.26.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct HeroB2WidgetsBundle: WidgetBundle {
+    var body: some Widget {
+        HeroB2Widgets()
+        HeroB2WidgetsControl()
+    }
+}
